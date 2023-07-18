caa7298ba68ea3b6529664121f3471b200e1600a
s22028 <s22028@std.it-college.ac.jp>
2023-07-18 14:03:44 +0900

    4章終わり。　ViewBinding　を使うように変更した。

--- HelloSample/app/src/main/java/jp/ac/it_college/std/s22028/hellosample/MainActivity.kt
+++ HelloSample/app/src/main/java/jp/ac/it_college/std/s22028/hellosample/MainActivity.kt
@@ -6,33 +6,49 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import jp.ac.it_college.std.s22028.hellosample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    /*
+    ViewBinding 機能を使って画面を作る方法
+    activity_main.xml ファイルに対して ActivityMainBinding クラスが
+    自動生成されます。（レイアウトファイル名 + Binding)
+     */
+    private  lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        // Binding クラスのインスタンスを生成いしてもらう
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        // Binding クラスが管理しているビューをつかう
+        //setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        
         // ボタンを取ってくる
-        val btclick = findViewById<Button>(R.id.btClick)
+//        val btclick = findViewById<Button>(R.id.btClick)
+        // findViewByID で取ってくるとか古すぎるのでしません
+
         // リスナクラスのインスタンスを作る
         val listener = HelloListener()
         // ボタンにリスナを設定する
-        btclick.setOnClickListener(listener)
+        binding.btClick.setOnClickListener(listener)
     }
 
     private inner class HelloListener: View.OnClickListener {
         override fun onClick(v: View?) {
             // EditText を取ってくる
-            val input = findViewById<EditText>(R.id.etName)
+//            val input = binding.etName
 
             // メッセージを出力する　TextView をとってくる
-            val output = findViewById<TextView>(R.id.tvOutput)
+//            val output = binding.tvOutput
 
             // input(EditText) から入力内容をとり出して String型へ
-            val inputStr = input.text.toString()
+//            val inputStr = input.text.toString()
+            val inputStr = binding.etName.text.toString()
 
             // メッセージをだす
-            output.text = "${inputStr}さん、こんにちは"
+//            output.text = "${inputStr}さん、こんにちは"
+            binding.tvOutput.text = "${inputStr}さん、こんにちは"
         }
 
     }
