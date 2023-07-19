ab01c052328c2171800b3adab43653b3a3cdaf1d
s22028 <s22028@std.it-college.ac.jp>
2023-07-19 16:11:28 +0900

    5章 ダイアログを表示させた

--- ListViewSample2/app/src/main/java/jp/ac/it_college/std/s22028/listviewsample2/MainActivity.kt
+++ ListViewSample2/app/src/main/java/jp/ac/it_college/std/s22028/listviewsample2/MainActivity.kt
@@ -1,8 +1,9 @@
 package jp.ac.it_college.std.s22028.listviewsample2
 
-import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import jp.ac.it_college.std.s22028.listviewsample2.databinding.ActivityMainBinding
 
@@ -35,5 +36,13 @@ class MainActivity : AppCompatActivity() {
         )
         val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, menuList)
         binding.lvMenu.adapter = adapter
+        binding.lvMenu.onItemClickListener = ListItemClickListener()
+    }
+    private inner class ListItemClickListener : AdapterView.OnItemClickListener {
+        override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+            val dialogFragment = OrderConfirmDialogFragment()
+            dialogFragment.show(supportFragmentManager, "OrderConfirmDialogFragment")
+        }
+
     }
 }
