25c81e3d3be18bd4b31b7a63fb2c8ef8b30ec58b
itc-s22028 <104543093+itc-s22028@users.noreply.github.com>
2023-08-10 14:15:44 +0900

    Update MainActivity.kt

--- MenuSample/app/src/main/java/jp/ac/it_college/std/s22028/menusample/MainActivity.kt
+++ MenuSample/app/src/main/java/jp/ac/it_college/std/s22028/menusample/MainActivity.kt
@@ -20,6 +20,8 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setSupportActionBar(binding.toolbar2)
+        
         initList(binding.lvMenu)
     }
     private fun initList(view: RecyclerView) {
