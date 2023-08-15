57531af5b8a048a27924c8b6921745da86d28229
s22028 <s22028@std.it-college.ac.jp>
2023-08-15 14:04:11 +0900

    chap09 9.2までできた

--- FragmentSample/app/src/main/java/jp/ac/it_college/std/s22028/fragmentsample/MainActivity.kt
+++ FragmentSample/app/src/main/java/jp/ac/it_college/std/s22028/fragmentsample/MainActivity.kt
@@ -2,10 +2,14 @@ package jp.ac.it_college.std.s22028.fragmentsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import jp.ac.it_college.std.s22028.fragmentsample.databinding.ActivityMainBinding
+import jp.ac.it_college.std.s22028.fragmentsample.databinding.FragmentMenuListBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
     }
 }
